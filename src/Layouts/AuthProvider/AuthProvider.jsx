e9c372281e6cedd7349d92556bab29e4d8461654
MehediHasan4321 <126mehedi@gmail.com>
2023-05-30 14:26:18 +0600

    added jwt token

--- src/Layouts/AuthProvider/AuthProvider.jsx
+++ src/Layouts/AuthProvider/AuthProvider.jsx
@@ -1,37 +1,47 @@
 import { createContext, useEffect, useState } from "react";
-import {GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut} from 'firebase/auth'
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth'
 import app from "../../firebase/firebase.config";
+import axios from "axios";
 export const AuthContext = createContext(null)
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
     const auth = getAuth(app)
     const googleAuth = new GoogleAuthProvider()
-    const [user,setUser] = useState(null)
-    const [isLoading,setIsloading] = useState(true)
-    const signUpWithEmailPass = (email,password)=>{
+    const [user, setUser] = useState(null)
+    const [isLoading, setIsloading] = useState(true)
+    const signUpWithEmailPass = (email, password) => {
         setIsloading(true)
-        return createUserWithEmailAndPassword(auth,email,password)
+        return createUserWithEmailAndPassword(auth, email, password)
     }
-    const logInWithEmailPassword = (email,password) =>{
-        return signInWithEmailAndPassword(auth,email,password)
+    const logInWithEmailPassword = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
     }
-    const singUpWithGoogle = ()=>{
+    const singUpWithGoogle = () => {
         setIsloading(true)
-        return signInWithPopup(auth,googleAuth)
+        return signInWithPopup(auth, googleAuth)
     }
 
-    const logOut = ()=>{
+    const logOut = () => {
         return signOut(auth)
     }
-    useEffect(()=>{
-       const unsubscribe = onAuthStateChanged(auth,currentUser=>{
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser)
+            if (currentUser) {
+                axios.post('http://localhost:5000/jwt')
+                    .then(data => {
+                        localStorage.setItem('access_token', data.data.token)
+                    })
+            } else {
+                localStorage.removeItem('access_token')
+            }
+
             setIsloading(false)
         })
-        return ()=> unsubscribe()
-    },[])
+        return () => unsubscribe()
+    }, [])
 
     const authInfo = {
-        user ,
+        user,
         singUpWithGoogle,
         signUpWithEmailPass,
         logOut,
